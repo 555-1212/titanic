40799d5865ba3dd24d10e4d7295af23913ac4b34
555-1212 <gerric@protonmail.com>
2017-05-29 16:43:44 -0400

    latest

--- titanic.R
+++ titanic.R
@@ -10,12 +10,14 @@ full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
 # Show title counts by sex
 table(full$Sex, full$Title)
 # Titles with very low cell counts to be combined to "rare" level
-rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
-                'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
+# rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
+#                 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
+# rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
+#                 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
 # Also reassign mlle, ms, and mme accordingly
-full$Title[full$Title == 'Mlle']        <- 'Miss' 
+full$Title[full$Title == 'Mlle']        <- 'Miss'
 full$Title[full$Title == 'Ms']          <- 'Miss'
-full$Title[full$Title == 'Mme']         <- 'Mrs' 
+full$Title[full$Title == 'Mme']         <- 'Mrs'
 full$Title[full$Title %in% rare_title]  <- 'Rare Title'
 # Show title counts by sex again
 table(full$Sex, full$Title)
